4c99800b10ace204988a141f735996cf57de146d
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-08 15:04:56 +0200

    Load courses from the API

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,11 +7,17 @@ import { GoSearch } from 'react-icons/go';
 import Navbar from '../components/common/Navbar';
 import CoursesList from './CoursesList';
 import Counter from '../components/common/Counter';
+import { loadCoursesAsync } from '../store/thunks/coursesThunk';
 
 const Home = (props) => {
-  const { history, currentUser } = props;
-  useEffect(() => (currentUser ? '' : history.replace('/login')));
-
+  const {
+    history, currentUser, courses, loadCourses,
+  } = props;
+  useEffect(() => {
+    if (!currentUser) return history.replace('/login');
+    return loadCourses();
+  }, []);
+  console.log(courses);
   return (
     <div className="home-main-container">
       <div className="header">
@@ -39,10 +45,17 @@ Home.defaultProps = {
 Home.propTypes = {
   history: PropTypes.objectOf(PropTypes.any).isRequired,
   currentUser: PropTypes.objectOf(PropTypes.any),
+  courses: PropTypes.arrayOf(PropTypes.any).isRequired,
+  loadCourses: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   currentUser: state.auth.currentUser,
+  courses: state.courses.list,
 });
 
-export default connect(mapStateToProps)(Home);
+const mapDispatchToProps = {
+  loadCourses: () => loadCoursesAsync(),
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store/reducers/courses.js
+++ src/store/reducers/courses.js
@@ -1,3 +1,9 @@
+import {
+  API_CALL_BEGIN,
+  LOAD_COURSES_FAILURE,
+  LOAD_COURSES_SUCCESS,
+} from '../actions/actionTypes';
+
 const initialState = {
   list: [],
   loading: false,
@@ -6,8 +12,25 @@ const initialState = {
 
 const courses = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_COURSE':
-      return state;
+    case API_CALL_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case LOAD_COURSES_SUCCESS:
+      return {
+        ...state,
+        list: action.payload,
+        loading: false,
+        error: null,
+      };
+    case LOAD_COURSES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
