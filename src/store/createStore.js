509532c308f1f70093dda6465e09c8ab6c58ae14
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-15 18:12:26 +0200

    Fix the bug with the auth token

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const store = () => createStore(reducer, applyMiddleware(thunk));
+const store = () => createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
